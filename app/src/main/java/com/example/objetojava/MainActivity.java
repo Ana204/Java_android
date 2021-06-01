38704b6746825470d608f94943f3b4f381c4da39
ana204 <anagomes.lucia@hotmail.com>
2021-06-01 13:02:38 -0300

    class carro

--- app/src/main/java/com/example/objetojava/MainActivity.java
+++ app/src/main/java/com/example/objetojava/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //chamo a minha class
-    public Veiculo veiculo;
+    private Carro carro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //instanciando meu objeto
-        veiculo = new Veiculo("Vermelho", "4", "FORD");
+        carro = new Carro("Verde", "4", "FORD");
 
-
-        veiculo.setCor("Azul");
-        veiculo.setRodas("4");
-        veiculo.setMarca("FIAT");
-
-        System.out.println(veiculo.getCor() + " - " + veiculo.getRodas() + " - " + veiculo.getMarca());
-
-
+        System.out.println(carro.getCor() + " - " + carro.getRodas() + " - " + carro.getMarca());
     }
 }
